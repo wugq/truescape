4c6123bb19f4726147a9c835f4915d073482d4fa
WuGuoquan <wugq.dev@gmail.com>
2022-11-23 15:36:44 +1300

    add three.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { NgxThreeModule } from 'ngx-three';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ThreeComponent } from './three/three.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, LoginComponent, HomeComponent, ThreeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxThreeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
